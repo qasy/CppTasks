3de1f5f3005ae4d14cd6f7af90ac5dca08780e1f
aivanov <aleksey.ivanov@apstecsystems.com>
2024-05-02 13:01:07 +0300

    update with another commands

--- SimpleCode/Lessons/ConditionalCompilation/main.cpp
+++ SimpleCode/Lessons/ConditionalCompilation/main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
+
 #define DEBUG
+#ifdef DEBUG
+#define DEBUG_LEVEL 5
+#endif
 
 int main()
 {
 
 #ifdef DEBUG
-    std::cout << "Debug: Start of cycle For" << std::endl;
+#if DEBUG_LEVEL > 2
+    std::cout << "Debug level is: " << DEBUG_LEVEL << " Start of cycle For" << std::endl;
+#elif DEBUG_LEVEL > 1
+    std::cout << "Debug level is: " << DEBUG_LEVEL << " Start of cycle For" << std::endl;
+#else
+    std::cout << "Debug is off:" << std::endl;
+#endif
 #endif // DEBUG
 
     for (size_t i = 0; i < 10; ++i)
@@ -15,7 +25,9 @@ int main()
     std::cout << std::endl;
 
 #ifdef DEBUG
-    std::cout << "Debug: End of cycle For" << std::endl;
+    std::cout << "Debug is on: End of cycle For" << std::endl;
+#else
+    std::cout << "Debug is off:" << std::endl;
 #endif // DEBUG
 
     return 0;
