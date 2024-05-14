35a6b64c9c9e3e132d6dc6f58cda345515680cb8
aivanov <aleksey.ivanov@apstecsystems.com>
2024-05-14 16:04:44 +0300

    some update

--- SimpleCode/Lessons/DelegatingConstructor/main.cpp
+++ SimpleCode/Lessons/DelegatingConstructor/main.cpp
@@ -1,10 +1,35 @@
 #include <iostream>
+#include <string>
 
 class Human
 {
+public:
+    Human(std::string name)
+    {
+        this->Name   = name;
+        this->Age    = 0;
+        this->Weight = 0;
+    }
+
+    Human(std::string name, int age)
+        : Human(name)
+    {
+        this->Age = age;
+    }
+
+    Human(std::string name, int age, int weight)
+        : Human(name, age)
+    {
+        this->Weight = weight;
+    }
+
+    std::string Name;
+    int Age;
+    int Weight;
 };
 
 int main()
 {
+    Human h("Alex", 25, 80);
     return 0;
 }
