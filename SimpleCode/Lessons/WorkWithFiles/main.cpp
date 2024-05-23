7e73a3cb6c1c920e165711cb9b1b4e755a5f3e06
aivanov <aleksey.ivanov@apstecsystems.com>
2024-05-23 08:55:52 +0300

    update

--- SimpleCode/Lessons/WorkWithFiles/main.cpp
+++ SimpleCode/Lessons/WorkWithFiles/main.cpp
@@ -12,16 +12,29 @@
 int main()
 {
 
-    std::ofstream outfilestream;
     std::string fname = "my_file.txt";
+
+    // write something to file
+    std::ofstream fout;
     // outfilestream.open(fname); // Если файл есть - то перезаписываем, если нет - создаем новый
-    outfilestream.open(fname, std::fstream::app); // Если файл есть - то добавляем в конец, если нет - создаем новый
-    if (!outfilestream.is_open())
+    fout.open(fname, std::fstream::app); // Если файл есть - то добавляем в конец, если нет - создаем новый
+    if (!fout.is_open())
     {
-        std::cout << "Error, the file is not open." << std::endl;
+        std::cout << "Ошибка открытия файла." << std::endl;
     }
-    outfilestream << "Success. File was opened." << std::endl;
-    outfilestream.close();
+    fout << "Success. File was opened." << std::endl;
+    fout.close();
+
+    // read something from file
+    std::ifstream fin;
+    fin.open(fname);
+
+    if (!fin.is_open())
+    {
+        std::cout << "Ошибка открытия файла." << std::endl;
+        return 1;
+    }
+    std::cout << "The file is open and readable:" << std::endl;
 
     return 0;
 }
