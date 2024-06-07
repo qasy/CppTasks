12963fb61ffbdb192bba85c39e1527942474d5a4
aivanov <aleksey.ivanov@apstecsystems.com>
2024-06-07 12:37:47 +0300

    update

--- Games/ConsoleSnake/main.cpp
+++ Games/ConsoleSnake/main.cpp
@@ -1,6 +1,9 @@
+// #include "SharedResource.h"
 #include <chrono>
 #include <fstream>
 #include <iostream>
+#include <memory>
+#include <mutex>
 #include <thread>
 #include <vector>
 
@@ -65,6 +68,16 @@
 // private:
 // };
 
+std::mutex msg_mtx;
+
+void LogMultithreading(const std::string& msg)
+{
+    std::unique_lock<std::mutex> unique_guard(msg_mtx);
+    std::cout << "thread (id:" << std::this_thread::get_id() << ")\t";
+    std::cout << msg << std::endl;
+    unique_guard.unlock();
+}
+
 class Snake
 {
 public:
@@ -75,14 +88,21 @@ public:
 
     void Move()
     {
+        // std::cin.get()
     }
 
     void printSnakeCoordinates() const
     {
         for (auto& el : this->SnakeBody)
         {
-            std::cout << "(" << el.first << "," << el.second << ")" << std::endl;
+            std::string msg = "(" + std::to_string(el.first) + "," + std::to_string(el.second) + ")";
+            LogMultithreading(msg);
         }
+    }
+
+    const std::vector<std::pair<int, int>>& GetPositions() const
+    {
+        return SnakeBody;
     }
 
 private:
@@ -97,49 +117,112 @@ void GameCycle()
     auto delta_time_ms  = std::chrono::duration_cast<std::chrono::milliseconds>(current_time - start_time).count();
 
     // Game cycle
-
     std::cout << "Start GameCycle()" << std::endl;
     for (; delta_time_ms < duration_ms;)
     {
         delta_time_ms = std::chrono::duration_cast<std::chrono::milliseconds>(current_time - start_time).count();
-        std::cout << "thread: " << std::this_thread::get_id() << "\t time: " << delta_time_ms << std::endl;
 
-        std::this_thread::sleep_for(std::chrono::milliseconds(500));
+        LogMultithreading("time: " + std::to_string(delta_time_ms));
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(200));
         current_time = std::chrono::system_clock::now();
     }
     std::cout << "End GameCycle()" << std::endl;
 }
 
+class Frame;
+
 int main()
 {
-    Snake s(0, 0);
-    s.printSnakeCoordinates();
+    Snake snake(0, 0);
 
-    std::thread thread_gamecycle(&GameCycle);
+    // std::cin.get();
 
-    // std::string a;
-    // std::cin >> a;
-    // std::cout << a << std::endl;
+    // /////
+    std::unique_ptr<int> ptr_a;
+    std::unique_ptr<int[]> ptr_arr;
+    const int& rref = 5;
+    std::cout << rref << std::endl;
 
-    int64_t duration_ms = 20000; // duration of game cycle in seconds
-    auto start_time     = std::chrono::system_clock::now();
-    auto current_time   = std::chrono::system_clock::now();
-    auto delta_time_ms  = std::chrono::duration_cast<std::chrono::milliseconds>(current_time - start_time).count();
+    // std::cout << ptr_b << std::endl;
 
-    // Game cycle
+    // /////
 
-    std::cout << "Start MainCycle()" << std::endl;
-    for (; delta_time_ms < duration_ms;)
-    {
-        delta_time_ms = std::chrono::duration_cast<std::chrono::milliseconds>(current_time - start_time).count();
-        std::cout << "thread: " << std::this_thread::get_id() << "\t time: " << delta_time_ms << std::endl;
+    // snake.Move();
 
-        std::this_thread::sleep_for(std::chrono::milliseconds(500));
-        current_time = std::chrono::system_clock::now();
-    }
-    std::cout << "End MainCycle()" << std::endl;
+    // s.printSnakeCoordinates();
+    // Frame frame;
 
-    thread_gamecycle.join();
+    // std::thread thread_gamecycle(&GameCycle);
+
+    // // std::string a;
+    // // std::cin >> a;
+    // // std::cout << a << std::endl;
+
+    // int64_t duration_ms = 2000; // duration of game cycle in seconds
+    // auto start_time     = std::chrono::system_clock::now();
+    // auto current_time   = std::chrono::system_clock::now();
+    // auto delta_time_ms  = std::chrono::duration_cast<std::chrono::milliseconds>(current_time -
+    // start_time).count();
+
+    // for (; delta_time_ms < duration_ms;)
+    // {
+    //     delta_time_ms = std::chrono::duration_cast<std::chrono::milliseconds>(current_time - start_time).count();
+    //     LogMultithreading("time: " + std::to_string(delta_time_ms));
+
+    //     std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    //     current_time = std::chrono::system_clock::now();
+    // }
+
+    // thread_gamecycle.join();
 
     return 0;
 }
+
+class Frame
+{
+public:
+    void SetSnakeData(Snake& snake)
+    {
+        // snake
+    }
+
+    /**
+     * @brief Use compiler-generated default constructor
+     *
+     */
+    Frame() = default;
+
+    /**
+     * @brief Use compiler-generated default destructor
+     *
+     */
+    ~Frame() = default;
+
+    /**
+     * @brief The class is non-copyable
+     *
+     */
+    Frame(const Frame& other) = delete;
+
+    /**
+     * @brief The class is non-copyable
+     *
+     */
+    Frame& operator=(const Frame& other) = delete;
+
+    /**
+     * @brief The class is non-movable
+     *
+     */
+    Frame(Frame&& other) = delete;
+
+    /**
+     * @brief The class is non-movable
+     *
+     */
+    Frame& operator=(Frame&& other) = delete;
+
+private:
+    std::vector<std::pair<int, int>> snake;
+};
